58ab857b6ea4809b8283125037b216f3f1e41e3b
YiPana <37297544+YiPana@users.noreply.github.com>
2018-07-02 03:31:39 +0300

    Knockout phase added till end

--- src/russiawc/RussiaWC.java
+++ src/russiawc/RussiaWC.java
@@ -31,6 +31,7 @@ public class RussiaWC {
         String[][] G = {{"Belgium", "8.8", "0", "0", "0"}, {"Panama", "3.1", "0", "0", "0"}, {"Tunisia", "4.8", "0", "0", "0"}, {"England", "8.0", "0", "0", "0"}};
         String[][] H = {{"Poland", "6.8", "0", "0", "0"}, {"Senegal", "6.9", "0", "0", "0"}, {"Colombia", "7.3", "0", "0", "0"}, {"Japan", "7.0", "0", "0", "0"}};
 
+        //***********************************************************GROUP STAGE*********************************************************
         System.out.println("GROUP A\n");
         Group Group_A = new Group(A[0][0], Double.valueOf(A[0][1]), A[1][0], Double.valueOf(A[1][1]),
                 A[2][0], Double.valueOf(A[2][1]), A[3][0], Double.valueOf(A[3][1]));
@@ -55,12 +56,117 @@ public class RussiaWC {
         Group Group_F = new Group(F[0][0], Double.valueOf(F[0][1]), F[1][0], Double.valueOf(F[1][1]),
                 F[2][0], Double.valueOf(F[2][1]), F[3][0], Double.valueOf(F[3][1]));
         Group_F.Results();
+        System.out.println("GROUP G\n");
+        Group Group_G = new Group(G[0][0], Double.valueOf(G[0][1]), G[1][0], Double.valueOf(G[1][1]),
+                G[2][0], Double.valueOf(G[2][1]), G[3][0], Double.valueOf(G[3][1]));
+        Group_G.Results();
         System.out.println("GROUP H\n");
         Group Group_H = new Group(H[0][0], Double.valueOf(H[0][1]), H[1][0], Double.valueOf(H[1][1]),
                 H[2][0], Double.valueOf(H[2][1]), H[3][0], Double.valueOf(H[3][1]));
         Group_H.Results();
 
-        System.out.println("First team " + Group_A.get_Team1() + "\t" + Group_A.get_rating1());
-        System.out.println("Second team " + Group_A.get_Team2() + "\t" + Group_A.get_rating2());
+        System.out.println("Group A - 1st team : " + String.format("%1$-20s", Group_A.get_Team1()) + "\t" + Group_A.get_rating1());
+        System.out.println("Group A - 2nd team : " + String.format("%1$-20s", Group_A.get_Team2()) + "\t" + Group_A.get_rating2());
+        
+        System.out.println("Group B - 1st team : " + String.format("%1$-20s", Group_B.get_Team1()) + "\t" + Group_B.get_rating1());
+        System.out.println("Group B - 2nd team : " + String.format("%1$-20s", Group_B.get_Team2()) + "\t" + Group_B.get_rating2());
+        
+        System.out.println("Group C - 1st team : " + String.format("%1$-20s", Group_C.get_Team1()) + "\t" + Group_C.get_rating1());
+        System.out.println("Group C - 2nd team : " + String.format("%1$-20s", Group_C.get_Team2()) + "\t" + Group_C.get_rating2());
+        
+        System.out.println("Group D - 1st team : " + String.format("%1$-20s", Group_D.get_Team1()) + "\t" + Group_D.get_rating1());
+        System.out.println("Group D - 2nd team : " + String.format("%1$-20s", Group_D.get_Team2()) + "\t" + Group_D.get_rating2());
+        
+        System.out.println("Group E - 1st team : " + String.format("%1$-20s", Group_E.get_Team1()) + "\t" + Group_E.get_rating1());
+        System.out.println("Group E - 2nd team : " + String.format("%1$-20s", Group_E.get_Team2()) + "\t" + Group_E.get_rating2());
+        
+        System.out.println("Group F - 1st team : " + String.format("%1$-20s", Group_F.get_Team1()) + "\t" + Group_F.get_rating1());
+        System.out.println("Group F - 2nd team : " + String.format("%1$-20s", Group_F.get_Team2()) + "\t" + Group_F.get_rating2());
+        
+        System.out.println("Group G - 1st team : " + String.format("%1$-20s", Group_G.get_Team1()) + "\t" + Group_G.get_rating1());
+        System.out.println("Group G - 2nd team : " + String.format("%1$-20s", Group_G.get_Team2()) + "\t" + Group_G.get_rating2());
+        
+        System.out.println("Group H - 1st team : " + String.format("%1$-20s", Group_H.get_Team1()) + "\t" + Group_H.get_rating1());
+        System.out.println("Group H - 2nd team : " + String.format("%1$-20s", Group_H.get_Team2()) + "\t" + Group_H.get_rating2());
+        
+        //********************************************************KNOCKOUT 16*******************************************************
+        System.out.println("\n\n\t\t\t\t PHASE OF KNOCKOUT 16\n");
+        SimResult knockout = new SimResult();
+        
+        knockout.GetKnockoutResult(Group_A.get_Team1(), Group_A.get_rating1(), Group_B.get_Team2(), Group_B.get_rating2());
+        String W_A1B2 = knockout.getWinnerTeam();
+        double r_A1B2 = knockout.getWinnerRating();
+        
+        knockout.GetKnockoutResult(Group_B.get_Team1(), Group_B.get_rating1(), Group_A.get_Team2(), Group_A.get_rating2());
+        String W_B1A2 = knockout.getWinnerTeam();
+        double r_B1A2 = knockout.getWinnerRating();
+        
+        knockout.GetKnockoutResult(Group_C.get_Team1(), Group_C.get_rating1(), Group_D.get_Team2(), Group_D.get_rating2());
+        String W_C1D2 = knockout.getWinnerTeam();
+        double r_C1D2 = knockout.getWinnerRating();
+        
+        knockout.GetKnockoutResult(Group_D.get_Team1(), Group_D.get_rating1(), Group_C.get_Team2(), Group_C.get_rating2());
+        String W_D1C2 = knockout.getWinnerTeam();
+        double r_D1C2 = knockout.getWinnerRating();
+        
+        knockout.GetKnockoutResult(Group_E.get_Team1(), Group_E.get_rating1(), Group_F.get_Team2(), Group_F.get_rating2());
+        String W_E1F2 = knockout.getWinnerTeam();
+        double r_E1F2 = knockout.getWinnerRating();
+        
+        knockout.GetKnockoutResult(Group_F.get_Team1(), Group_F.get_rating1(), Group_E.get_Team2(), Group_E.get_rating2());
+        String W_F1E2 = knockout.getWinnerTeam();
+        double r_F1E2 = knockout.getWinnerRating();
+        
+        knockout.GetKnockoutResult(Group_G.get_Team1(), Group_G.get_rating1(), Group_H.get_Team2(), Group_H.get_rating2());
+        String W_G1H2 = knockout.getWinnerTeam();
+        double r_G1H2 = knockout.getWinnerRating();
+        
+        knockout.GetKnockoutResult(Group_H.get_Team1(), Group_H.get_rating1(), Group_G.get_Team2(), Group_G.get_rating2());
+        String W_H1G2 = knockout.getWinnerTeam();
+        double r_H1G2 = knockout.getWinnerRating();
+        
+        //************************************************QUARTER FINALS ******************************************************
+        System.out.println("\n\n\t\t\t\t PHASE OF QUARTER FINALS\n");
+        //QF_A1B2_C1D2
+        knockout.GetKnockoutResult(W_A1B2, r_A1B2, W_C1D2, r_C1D2);
+        String Q1 = knockout.getWinnerTeam();
+        double r_Q1 = knockout.getWinnerRating();
+        //QF_B1A2_D1C2
+        knockout.GetKnockoutResult(W_B1A2, r_B1A2, W_D1C2, r_D1C2);
+        String Q3 = knockout.getWinnerTeam();
+        double r_Q3 = knockout.getWinnerRating();
+        //QF_E1F2_G1H2
+        knockout.GetKnockoutResult(W_E1F2, r_E1F2, W_G1H2, r_G1H2);
+        String Q2 = knockout.getWinnerTeam();
+        double r_Q2 = knockout.getWinnerRating();
+        //QF_F1E2_H1G2
+        knockout.GetKnockoutResult(W_F1E2, r_F1E2, W_H1G2, r_H1G2);
+        String Q4 = knockout.getWinnerTeam();
+        double r_Q4 = knockout.getWinnerRating();
+        
+        //*************************************************SEMI FINALS************************************************************
+        System.out.println("\n\n\t\t\t\t PHASE OF SEMI FINALS\n");
+        //SF1
+        knockout.GetKnockoutResult(Q1, r_Q1, Q2, r_Q2);
+        String SF1_W = knockout.getWinnerTeam();
+        double r_SF1_W = knockout.getWinnerRating();
+        String SF1_L = knockout.getDefeatedTeam();
+        double r_SF1_L = knockout.getDefeatedRating();
+        //SF2
+        knockout.GetKnockoutResult(Q3, r_Q3, Q4, r_Q4);
+        String SF2_W = knockout.getWinnerTeam();
+        double r_SF2_W = knockout.getWinnerRating();
+        String SF2_L = knockout.getDefeatedTeam();
+        double r_SF2_L = knockout.getDefeatedRating();
+        
+        //**************************************************THIRD PLACE***********************************************************
+        System.out.println("\n\n\t\t\t\t THIRD PLACE\n");
+        knockout.GetKnockoutResult(SF1_L, r_SF1_L, SF2_L, r_SF2_L);
+        
+        //*****************************************************FINAL**************************************************************
+        System.out.println("\n\n\t\t\t\t FINAL\n");
+        knockout.GetKnockoutResult(SF1_W, r_SF1_W, SF2_W, r_SF2_W);
+        
+        System.out.println("Russia World Cup 2018 WINNER : " + knockout.getWinnerTeam() + "\n");
     }
 }
